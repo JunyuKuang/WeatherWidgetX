76c6a3afea34b13cc242d9835d876a31004923e5
Jonny Kuang <jykuang999@gmail.com>
2017-07-06 14:12:28 +0800

    Initial Commit

---
+++ WeatherWidgetX/AppDelegate.swift
@@ -0,0 +1,101 @@
+//
+//  AppDelegate.swift
+//  WeatherWidgetX
+//
+//  Created by Jonny on 7/5/17.
+//  Copyright © 2017 Jonny. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	let statusItem: NSStatusItem = {
+		let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+		statusItem.button?.title = "AQI"
+		
+		let menu = NSMenu()
+		let refreshItem = NSMenuItem(title: "Refresh", action: #selector(tapRefreshMenuItem), keyEquivalent: "R")
+		let exitItem = NSMenuItem(title: "Exit", action: #selector(tapExitMenuItem), keyEquivalent: "Q")
+		menu.addItem(refreshItem)
+		menu.addItem(exitItem)
+		statusItem.menu = menu
+		
+		return statusItem
+	}()
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		refresh()
+		Timer.scheduledTimer(timeInterval: 10 * 60, target: self, selector: #selector(refresh), userInfo: nil, repeats: true)
+	}
+	
+	@objc func refresh() {
+		
+		let url = URL(string: "http://guolin.tech/api/weather?cityid=CN101250101&key=bc0418b57b2d4918819d3974ac1285d9")!
+		
+		URLSession.shared.dataTask(with: url) { data, _, error in
+			if let data = data {
+				do {
+					let weathers = try JSONDecoder().decode(Weathers.self, from: data)
+					guard let weather = weathers.weathers.last else { return }
+					DispatchQueue.main.async {
+						self.updateUI(with: weather)
+					}
+				} catch {
+					print(error)
+					DispatchQueue.main.async {
+						let alert = NSAlert(error: error)
+						alert.runModal()
+					}
+				}
+			} else if let error = error {
+				print(error)
+				DispatchQueue.main.async {
+					let alert = NSAlert(error: error)
+					alert.runModal()
+				}
+			}
+		}.resume()
+	}
+	
+	func updateUI(with weather: Weather) {
+		statusItem.button?.title = weather.now.temperature + "°C" + ", " + weather.aqi.city.aqi + " " + weather.aqi.city.quality
+		
+		let forecasts = weather.dailyForecasts.map {
+			DateFormatter.localizedString(from: $0.dateComponents!.date!, dateStyle: .medium, timeStyle: .none) + "  " + $0.temperature.min + "°C" + " - " + $0.temperature.max + "°C  " + $0.more.info
+		}
+		let forecastMenuItems = forecasts.map { NSMenuItem(title: $0, action: #selector(tapGeneralItem), keyEquivalent: "") }
+		
+		let aqis = [
+			"AQI " + weather.aqi.city.aqi,
+			"PM2.5 " + weather.aqi.city.pm25,
+			"PM10 " + weather.aqi.city.pm10,
+		]
+		let aqiMenuItems = aqis.map { NSMenuItem(title: $0, action: #selector(tapGeneralItem), keyEquivalent: "") }
+		
+		let menu = NSMenu()
+		let items = [NSMenuItem(title: "Updated at " + DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .medium), action: nil, keyEquivalent: "")]
+			+ [NSMenuItem.separator()]
+			+ forecastMenuItems
+			+ [NSMenuItem.separator()]
+			+ aqiMenuItems
+			+ [NSMenuItem.separator()]
+			+ [NSMenuItem(title: "Refresh", action: #selector(tapRefreshMenuItem), keyEquivalent: "R"),
+			   NSMenuItem(title: "Exit", action: #selector(tapExitMenuItem), keyEquivalent: "Q")]
+		
+		items.forEach(menu.addItem)
+		statusItem.menu = menu
+	}
+	
+	@objc func tapGeneralItem(_ sender: NSMenuItem) {}
+	
+	@objc func tapRefreshMenuItem(_ sender: NSMenuItem) {
+		refresh()
+	}
+	
+	@objc func tapExitMenuItem(_ sender: NSMenuItem) {
+		NSApp.terminate(self)
+	}
+}
+
---
+++ WeatherWidgetX/Weather.swift
@@ -0,0 +1,185 @@
+//
+//  Weather.swift
+//  WeatherWidgetX
+//
+//  Created by Jonny on 7/6/17.
+//  Copyright © 2017 Jonny. All rights reserved.
+//
+
+import Foundation
+
+//struct AQI : Codable {
+//
+//	struct Data : Codable {
+//		let aqi: Int
+//	}
+//
+//	let data: Data
+//	let status: String
+//}
+//
+
+struct Weathers : Codable {
+	let weathers: [Weather]
+	private enum CodingKeys : String, CodingKey {
+		case weathers = "HeWeather"
+	}
+}
+
+struct Weather : Codable {
+	
+	let status: String
+	let basic: Basic
+	let aqi: AQI
+	let now: Now
+	let suggestion: Suggestion
+	let dailyForecasts: [Forecast]
+	
+	private enum CodingKeys : String, CodingKey {
+		case status
+		case basic
+		case aqi
+		case now
+		case suggestion
+		case dailyForecasts = "daily_forecast"
+	}
+	
+	struct Basic : Codable {
+		let city: String
+		let id: String
+		let update: Update
+		
+		struct Update : Codable {
+			let updateTime: String
+			private enum CodingKeys : String, CodingKey {
+				case updateTime = "loc"
+			}
+		}
+	}
+	
+	struct AQI : Codable {
+		let city: City
+		
+		struct City : Codable {
+			let aqi: String
+			let co: String
+			let no2: String
+			let o3: String
+			let pm10: String
+			let pm25: String
+			let so2: String
+			
+			var quality : String {
+				guard let intValue = Int(aqi) else { return "" }
+				switch intValue {
+				case 0...50:
+					return "Excellent"
+				case 51...100:
+					return "Good"
+				case 101...150:
+					return "Lightly Polluted"
+				case 151...200:
+					return "Moderately Polluted"
+				case 201...300:
+					return "Heavily Polluted"
+				case 301...500:
+					return "Severely Polluted"
+				default:
+					return ""
+				}
+			}
+		}
+	}
+	
+	struct Now : Codable {
+		let temperature: String
+		let more: More
+		private enum CodingKeys : String, CodingKey {
+			case temperature = "tmp"
+			case more = "cond"
+		}
+		
+		struct More : Codable {
+			let info: String
+			private enum CodingKeys : String, CodingKey {
+				case info = "txt"
+			}
+		}
+	}
+	
+	struct Suggestion : Codable {
+		let comfort: Comfort
+		let carWash: Carwash
+		let sport: Sport
+		
+		private enum CodingKeys : String, CodingKey {
+			case comfort = "comf"
+			case carWash = "cw"
+			case sport
+		}
+		
+		struct Comfort : Codable {
+			let info: String
+			private enum CodingKeys : String, CodingKey {
+				case info = "txt"
+			}
+		}
+		
+		struct Carwash : Codable {
+			let info: String
+			private enum CodingKeys : String, CodingKey {
+				case info = "txt"
+			}
+		}
+		
+		struct Sport : Codable {
+			let info: String
+			private enum CodingKeys : String, CodingKey {
+				case info = "txt"
+			}
+		}
+	}
+	
+	struct Forecast : Codable {
+		private let dateString: String
+		let temperature: Temperature
+		let more: More
+		
+		private enum CodingKeys : String, CodingKey {
+			case dateString = "date"
+			case temperature = "tmp"
+			case more = "cond"
+		}
+		
+		struct Temperature : Codable {
+			let max: String
+			let min: String
+		}
+		
+		struct More : Codable {
+			let info: String
+			private enum CodingKeys : String, CodingKey {
+				case info = "txt_d"
+			}
+		}
+		
+		var dateComponents: DateComponents? {
+			let components = dateString.components(separatedBy: "-")
+			guard components.count == 3 else { return nil }
+			
+			guard let year = Int(components[0]),
+				let month = Int(components[1]),
+				let day = Int(components[2]) else { return nil }
+			
+			return DateComponents(calendar: Calendar(identifier: .gregorian),
+			                      timeZone: TimeZone(abbreviation: "Asia/Hong_Kong"),
+			                      year: year,
+			                      month: month,
+			                      day: day)
+		}
+	}
+}
+
+
+
+
